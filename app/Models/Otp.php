71559205b693569689f0bf1c359011c55867a793
rooneyvn <phamvannguyen.haui@gmail.com>
2022-07-25 14:00:21 +0700

    add default otp

--- app/Models/Otp.php
+++ app/Models/Otp.php
@@ -20,6 +20,9 @@ class Otp extends Model{
     const OTP_EXPIRE = 3; //3 phút
     const MAX_OTP_BY_PHONE_PER_DAY = 10; //gửi tối đa 10 otp cho 1 số điện thoại trong 1 ngày
 
+    private static $__default_phone_otp_arr = ['0392565507','0963760289','0587113333'];
+    private static $_default_otp = '123456';
+
 
     /**
      * @param $phone
@@ -88,7 +91,11 @@ class Otp extends Model{
     }
 
     public static function genOtp($phone='', $length = self::OTP_LENGTH){
-        return rand(100000,999999);
+        if(in_array($phone, self::$__default_phone_otp_arr)){
+            return self::$_default_otp;
+        }else {
+            return rand(100000, 999999);
+        }
     }
 
     private static function __countOtpSentByPhone($phone){
