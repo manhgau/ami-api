919679c954c54ea998a9c21b1d193780f032b94f
rooneyvn <phamvannguyen.haui@gmail.com>
2022-08-02 13:44:29 +0700

    api update partner profile - DONE

--- app/Models/PartnerProfile.php
+++ app/Models/PartnerProfile.php
@@ -7,9 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 class PartnerProfile extends Model
 {
     protected $fillable = [
-        'name',
-        'min_value',
-        'max_value',
+        'partner_id',
+        'point',
+        'kpi_point',
+        'year_of_birth',
+        'gender',
+        'province_code',
+        'district_code',
+        'ward_code',
+        'job_type_id',
+        'job_status_id',
+        'academic_level_id',
+        'marital_status_id',
+        'personal_income_level_id',
+        'family_income_level_id',
+        'family_people',
+        'has_children',
+        'is_key_shopper',
+        'most_cost_of_living',
+        'note',
     ];
 
     const STATUS_ACTIVE = 1;
