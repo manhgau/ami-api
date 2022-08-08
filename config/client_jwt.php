9ad1797b6ffb5f87eab1ebdfeceb1d7e4cbfc1f7
rooneyvn <phamvannguyen.haui@gmail.com>
2022-08-08 14:19:47 +0700

    change access, refresh time

--- config/client_jwt.php
+++ config/client_jwt.php
@@ -6,6 +6,6 @@
  * Time: 14:09
  */
 return [
-    'ttl' => env('CLIENT_JWT_TTL', 2),    //2 phút
-    'refresh_ttl' => env('CLIENT_JWT_REFRESH_TTL', 10), //10 phút
+    'ttl' => env('CLIENT_JWT_TTL', 15),    //15 phút
+    'refresh_ttl' => env('CLIENT_JWT_REFRESH_TTL', 43200), //30 ngày, tính theo số phút
 ];
