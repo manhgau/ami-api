7e05162db5d8c8c4287d1b6c7fac991e18cc5bc2
rooneyvn <phamvannguyen.haui@gmail.com>
2022-08-18 09:21:13 +0700

    model QuestionType

---
+++ app/Models/QuestionType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+/*use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;*/
+
+class QuestionType
+{
+
+    const MULTI_CHOICE_CHECKBOX                 = 'checkbox';
+    const MULTI_CHOICE_RADIO                    = 'radio';
+    const MULTI_CHOICE_DROPDOWN                 = 'dropdown';
+
+    const DATETIME_DATE                         = 'date';
+    const DATETIME_DATE_RANGE                   = 'date_range';
+
+    const QUESTION_ENDED_SHORT_TEXT             = 'text_box';
+    const QUESTION_ENDED_LONG_TEXT              = 'char_box';
+
+    const RATING_STAR                           = 'star_rating';
+
+
+    const MULTI_FACTOR_MATRIX                  = 'matrix';
+
+
+    public static function getTypeQuestionList(){
+        return [
+            self::MULTI_CHOICE_CHECKBOX,
+            self::MULTI_CHOICE_RADIO,
+            self::MULTI_CHOICE_DROPDOWN,
+            self::DATETIME_DATE,
+            self::DATETIME_DATE_RANGE,
+            self::QUESTION_ENDED_SHORT_TEXT,
+            self::QUESTION_ENDED_LONG_TEXT,
+            self::RATING_STAR,
+            self::MULTI_FACTOR_MATRIX
+        ];
+    }
+
+    public static function checkQuestionTypeValid($type){
+        $list = self::getTypeQuestionList();
+        if(in_array($type, $list)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
