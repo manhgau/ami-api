dd6076a16b31e82818b5c891ccf914134e6f09c3
manhgau <96876095+manhgau@users.noreply.github.com>
2023-02-03 21:45:40 +0700

    commit

--- app/Console/Commands/StateSurvey.php
+++ app/Console/Commands/StateSurvey.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class PushNotifications extends Command
+class StateSurvey extends Command
 {
 
     protected $signature = 'push-fire-notify';
@@ -13,20 +13,20 @@ class PushNotifications extends Command
 
     public function handle()
     {
-        try {
-            $start_time = microtime(true);
-            $this->line("Start");
-            $do_something = false;
-            $do_something = \App\Helpers\Firebase::notify();
-            if ($do_something == true) {
-                $this->line('Action complete');
-            } else {
-                $this->line('Nothing to do');
-            }
-            $end_time = microtime(true);
-            $this->line("Time: " . ($end_time - $start_time));
-            $this->line("Done");
-        } catch (\Exception $ex) {
-        }
+        // try {
+        //     $start_time = microtime(true);
+        //     $this->line("Start");
+        //     $do_something = false;
+        //     $do_something = \App\Helpers\Firebase::notify();
+        //     if ($do_something == true) {
+        //         $this->line('Action complete');
+        //     } else {
+        //         $this->line('Nothing to do');
+        //     }
+        //     $end_time = microtime(true);
+        //     $this->line("Time: " . ($end_time - $start_time));
+        //     $this->line("Done");
+        // } catch (\Exception $ex) {
+        // }
     }
 }
