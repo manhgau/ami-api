bb0d0880a30dd113c12c8fffcf11eaa3b6280ad4
manhgau <96876095+manhgau@users.noreply.github.com>
2023-03-27 15:22:17 +0700

    commit

---
+++ app/Helpers/DeleteProjectDataStorageExpires.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Helpers;
+
+use App\Models\Survey;
+use App\Models\SurveyPartnerInput;
+use App\Models\SurveyPartnerInputLine;
+use App\Models\UserPackage;
+use Carbon\Carbon;
+
+class DeleteProjectDataStorageExpires
+{
+    public static function deleteProjectDataStorageExpires()
+    {
+        $list_project = Survey::getAllSurvey();
+        if (is_array($list_project) && count($list_project) > 0) {
+            foreach ($list_project as $key => $value) {
+                $user_package = UserPackage::getPackageUser($value['user_id'], Carbon::now());
+                if ($user_package['data_storage'] > 0) {
+                    $time = Carbon::now()->addDays(-$user_package['data_storage'])->toDate()->format('Y-m-d');
+                    $start_time = date_format(date_create($value['start_time']), 'Y-m-d');
+                    if (strtotime($time) ==  strtotime($start_time)) {
+                        SurveyPartnerInput::deleteSurveyPartnerInput($value['id']);
+                        //SurveyPartnerInputLine::deleteSurveyPartnerInput($value['id']);
+                    };
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+}
