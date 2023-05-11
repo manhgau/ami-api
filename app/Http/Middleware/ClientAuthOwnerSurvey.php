41c47cd2e4ab5a868c5627c7a50cd85a9143bbcd
manhgau <96876095+manhgau@users.noreply.github.com>
2023-05-11 10:53:00 +0700

    commit

--- app/Http/Middleware/ClientAuthOwnerSurvey.php
+++ app/Http/Middleware/ClientAuthOwnerSurvey.php
@@ -6,6 +6,8 @@ namespace App\Http\Middleware;
 
 use App\Helpers\ClientResponse;
 use App\Helpers\Context;
+use App\Models\AppSetting;
+use App\Models\Images;
 use App\Models\Survey;
 use Closure;
 
@@ -17,6 +19,14 @@ class ClientAuthOwnerSurvey
         $id = $request->id ?? ($request->survey_id ?? '');
         $survey = Survey::getDetailSurvey($request->survey_id);
         if ($survey->state == Survey::STATUS_COMPLETED) {
+            $all_settings = AppSetting::getAllSetting();
+            $image_domain  = AppSetting::getByKey(AppSetting::IMAGE_DOMAIN, $all_settings);
+            $logo  = AppSetting::getByKey(AppSetting::LOGO, $all_settings);
+            if ($survey->background_id) {
+                $survey->background = $image_domain . Images::getDetailImage($survey->background_id)->image;
+            }
+            $survey->logo ? $survey->logo_default = 0 : $survey->logo_default = 1;
+            $survey->logo ? $survey->logo = $image_domain . $survey->logo : $survey->logo = $image_domain . $logo;
             return ClientResponse::responseError('Bản ghi đã hoàn thành, không được sửa bản ghi này',  $survey);
         }
         if (!$survey) {
